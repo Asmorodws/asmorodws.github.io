ea3d02a90a5cf8f437f7f2fa4442ef555f46af9a
Kylz1s <81004580+Asmorodws@users.noreply.github.com>
2023-11-03 02:09:31 +0700

    Create getJson.js

---
+++ getJson.js
@@ -0,0 +1,14 @@
+const defaultJson = {
+  pages: [{
+    name: "Name",
+    elements: [{
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text"
+    }, {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text"
+    }]
+  }]
+};
